4c1d1c0c5f87895b2bbc0056efde9886a61f2f77
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-11-21 21:18:56 +0000

    problems solved

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print("i run 2")
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        index = 1
+        ans = 0
+        for c in s:
+            alphabetValue = 123 - ord(c)
+            ans += alphabetValue*index
+            index+=1
+
+        return ans
+
+
+x = Solution()
+x.reverseDegree("zaza")
